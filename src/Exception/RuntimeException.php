9c284370f51119c85445c5c4fcbf8651e3df3d66
jmerta <jakub.merta@digitalcz.cz>
2020-10-07 12:34:46 +0200

    parse response body, set to exception variables

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -5,8 +5,41 @@ declare(strict_types=1);
 namespace DigitalCz\DigiSign\Exception;
 
 use RuntimeException as BaseRuntimeException;
+use Throwable;
 
 class RuntimeException extends BaseRuntimeException
 {
+    protected string $type;
+    protected string $title;
+    protected string $detail;
+    protected array $violations = [];
 
+    public function __construct($message = "", $code = 0, Throwable $previous = null) {
+        $decodedMessage = json_decode($message, true);
+        $this->type = $decodedMessage['type'];
+        $this->title = $decodedMessage['title'];
+        $this->detail = $decodedMessage['detail'];
+
+        if ($decodedMessage['violations']) {
+            $this->violations = $decodedMessage['violations'];
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getType(): string {
+        return $this->type;
+    }
+
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    public function getDetail(): string {
+        return $this->detail;
+    }
+
+    public function getViolations(): array {
+        return $this->violations;
+    }
 }
