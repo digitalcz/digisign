791646772300356008ecd46acd3d5dbc2de04e8b
mertic18 <47573820+mertic18@users.noreply.github.com>
2020-10-07 14:00:00 +0200

    parse response body, set to exception variables (#22)
    * parse response body, set to exception variables
    
    * fix code style - open brackets on new line
    
    * remove types from class attributes, add type specifiaction in comments
    
    * specify types in constructor, fix type of violations
    
    * specify type of violations
    
    Co-authored-by: jmerta <jakub.merta@digitalcz.cz>

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -5,8 +5,64 @@ declare(strict_types=1);
 namespace DigitalCz\DigiSign\Exception;
 
 use RuntimeException as BaseRuntimeException;
+use Throwable;
 
 class RuntimeException extends BaseRuntimeException
 {
+    /**
+     * @var string
+     */
+    protected $type;
 
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $detail;
+
+    /**
+     * @var array<mixed>
+     */
+    protected $violations = [];
+
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        $decodedMessage = json_decode($message, true);
+        $this->type = $decodedMessage['type'];
+        $this->title = $decodedMessage['title'];
+        $this->detail = $decodedMessage['detail'];
+
+        if ($decodedMessage['violations']) {
+            $this->violations = $decodedMessage['violations'];
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDetail(): string
+    {
+        return $this->detail;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function getViolations(): array
+    {
+        return $this->violations;
+    }
 }
