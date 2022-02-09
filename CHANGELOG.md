e49c837596c5f49c621431a9385e8c9a77d60711
Pavel Stejskal <spajxo@gmail.com>
2022-02-09 16:59:18 +0100

    Release 1.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes will be documented in this file.
 
 Updates should follow the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
 
-## [Unreleased]
+## [1.7.0]
 ### Added
 - Add `AccountCertificateEndpoint` and `Certificate` resource
 - Add `Label` resource and `LabelsEndpoint`
--- src/DigiSign.php
+++ src/DigiSign.php
@@ -27,7 +27,7 @@ use Psr\SimpleCache\CacheInterface;
 
 final class DigiSign implements EndpointInterface
 {
-    public const VERSION = '1.6.0';
+    public const VERSION = '1.7.0';
     public const API_BASE = 'https://api.digisign.org';
     public const API_BASE_TESTING = 'https://api.digisign.digital.cz';
 
