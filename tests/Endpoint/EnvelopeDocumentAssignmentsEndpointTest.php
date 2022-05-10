0d578b820e8295d67c8374c1192dd8c25c9c86d0
Libor Daněk <libor.danek@digital.cz>
2022-05-10 15:47:58 +0200

    [FTR] Added body assertion on EnvelopeDocumentAssignmentsEndpointTest.

--- tests/Endpoint/EnvelopeDocumentAssignmentsEndpointTest.php
+++ tests/Endpoint/EnvelopeDocumentAssignmentsEndpointTest.php
@@ -24,7 +24,12 @@ final class EnvelopeDocumentAssignmentsEndpointTest extends EndpointTestCase
             ],
         ]);
 
-        self::assertLastRequest('PUT', '/api/envelopes/bar/documents/assignments');
+        self::assertLastRequest('PUT', '/api/envelopes/bar/documents/assignments', [
+            '8a071df9-aa9a-4607-9bc4-1c148ae6d384' => [
+                'd73be485-731a-45b4-9566-489643952db0' => 'show',
+                'ed37dd22-cd24-47c6-ab22-f014bcd30c9d' => 'hide',
+            ],
+        ]);
     }
 
     protected static function endpoint(): EnvelopeDocumentAssignmentsEndpoint
