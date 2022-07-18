7ce2d86f1e6ec70b5b14599127b9fc868dc0fde5
Libor Daněk <libor.danek@digital.cz>
2022-07-18 12:47:42 +0200

    [REF] Removed EntityResourceTrait from EnvelopeDocumentSignatureSheets.

--- src/Resource/EnvelopeDocumentSignatureSheets.php
+++ src/Resource/EnvelopeDocumentSignatureSheets.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace DigitalCz\DigiSign\Resource;
 
-use DigitalCz\DigiSign\Resource\Traits\EntityResourceTrait;
-
 class EnvelopeDocumentSignatureSheets extends BaseResource
 {
-    use EntityResourceTrait;
-
     /** @var Collection<EnvelopeDocument> */
     public $documents;
 
