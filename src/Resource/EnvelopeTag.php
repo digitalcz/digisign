13185e9dbcec88c500a1271cf630867f5d9da90b
Pepperoni1337 <107676055+Pepperoni1337@users.noreply.github.com>
2023-10-02 07:53:38 +0200

    Update EnvelopeTag.php

--- src/Resource/EnvelopeTag.php
+++ src/Resource/EnvelopeTag.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace DigitalCz\DigiSign\Resource;
 
-use DateTime;
 use DigitalCz\DigiSign\Resource\Traits\EntityResourceTrait;
 
 class EnvelopeTag extends BaseResource
