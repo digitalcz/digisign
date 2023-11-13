347d6546085b53ae9c29829c2ecbd2488a3913f9
Pepperoni1337 <107676055+Pepperoni1337@users.noreply.github.com>
2023-11-13 15:34:25 +0100

    Dgs 1942 envelope template recipient defaults (#190)
    * Update EnvelopeTemplate.php
    
    Add visibleFields, validatedFields, delegation, signerDefaults, approverDefaults, ccDefaults
    
    * Create EnvelopeTemplateRecipientDefaults.php
    
    * Update EnvelopeTemplate.php
    
    * Update CHANGELOG.md

---
+++ src/Resource/EnvelopeTemplateRecipientDefaults.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DigitalCz\DigiSign\Resource;
+
+use DigitalCz\DigiSign\Resource\Traits\EntityResourceTrait;
+
+class EnvelopeTemplateRecipientDefaults extends BaseResource
+{
+    use EntityResourceTrait;
+
+    public string $signatureType;
+
+    public string $authenticationOnOpen;
+
+    public string $authenticationOnSignature;
+
+    public string $authenticationOnDownload;
+
+    public string $language;
+
+    public string $channelForSigner;
+
+    public string $channelForDownload;
+
+    /** @var array<string, string> */
+    public array $bankIdScopes;
+
+    /** @var array<string, string> */
+    public array $visibleFields;
+
+    /** @var array<string, string> */
+    public array $validatedFields;
+
+    public bool $delegation;
+
+    public ?string $scenario = null;
+
+    public ?string $identifyScenario = null;
+
+    public ?IdentifyScenarioInfo $identifyScenarioInfo = null;
+
+    public string $approvalMode;
+
+    public bool $approveDocumentsAtOnce;
+}
