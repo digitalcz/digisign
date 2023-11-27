5c392f37c068f4ebc19d5c2df66fa320d1d61e47
Pavel Stejskal <spajxo@gmail.com>
2023-11-27 10:44:57 +0100

    Add `MyAccount.createdAt` (#195)
    * Add `MyAccount.createdAt`
    
    * Update changelog

--- src/Resource/MyAccount.php
+++ src/Resource/MyAccount.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace DigitalCz\DigiSign\Resource;
 
+use DateTime;
+
 class MyAccount extends BaseResource
 {
     public string $id;
     public string $name;
     public string $status;
     public bool $active;
+    public DateTime $createdAt;
     public ?string $idpDomain;
     public ?string $shortName;
 }
