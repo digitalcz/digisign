44594b30f152c4396de4906947e4a88e0f3d8de1
David Kalianko <david.kalianko@digital.cz>
2024-06-17 18:11:22 +0200

    Extend `Contact` resource

--- src/Resource/Contact.php
+++ src/Resource/Contact.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DigitalCz\DigiSign\Resource;
 
+use DateTime;
 use DigitalCz\DigiSign\Resource\Traits\EntityResourceTrait;
 
 class Contact extends BaseResource
@@ -17,4 +18,18 @@ class Contact extends BaseResource
     public ?string $mobile = null;
 
     public string $language;
+
+    public ?string $address = null;
+
+    public ?DateTime $birthdate = null;
+
+    public ?string $birthnumber = null;
+
+    public ?string $company = null;
+
+    public ?string $identificationNumber = null;
+
+    public ?string $function = null;
+
+    public ?string $contractingParty = null;
 }
