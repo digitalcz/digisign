16eeb8791ff5d12988e28bb6f84c8da6469bee41
David Kalianko <david.kalianko@digital.cz>
2024-09-22 19:23:55 +0200

    Change Create endpoint from POST to PUT
    Create Endpoint now overwrite all items in BatchSending

--- src/Endpoint/BatchSendingItemsEndpoint.php
+++ src/Endpoint/BatchSendingItemsEndpoint.php
@@ -4,20 +4,26 @@ declare(strict_types=1);
 
 namespace DigitalCz\DigiSign\Endpoint;
 
-use DigitalCz\DigiSign\Endpoint\Traits\CRUDEndpointTrait;
+use DigitalCz\DigiSign\Endpoint\Traits\DeleteEndpointTrait;
+use DigitalCz\DigiSign\Endpoint\Traits\GetEndpointTrait;
+use DigitalCz\DigiSign\Endpoint\Traits\ListEndpointTrait;
+use DigitalCz\DigiSign\Endpoint\Traits\UpdateEndpointTrait;
 use DigitalCz\DigiSign\Resource\BatchSending;
 use DigitalCz\DigiSign\Resource\BatchSendingItem;
 
 /**
  * @extends ResourceEndpoint<BatchSendingItem>
  * @method BatchSendingItem get(string $id)
- * @method BatchSendingItem create(array $body)
  * @method BatchSendingItem update(string $id, array $body)
+ * @method BatchSendingItem delete(string $id)
  */
 final class BatchSendingItemsEndpoint extends ResourceEndpoint
 {
-    /** @use CRUDEndpointTrait<BatchSendingItem> */
-    use CRUDEndpointTrait;
+    /** @use ListEndpointTrait<BatchSendingItem> */
+    use ListEndpointTrait;
+    use GetEndpointTrait;
+    use UpdateEndpointTrait;
+    use DeleteEndpointTrait;
 
     public function __construct(BatchSendingsEndpoint $parent, BatchSending|string $batchSending)
     {
@@ -35,5 +41,13 @@ final class BatchSendingItemsEndpoint extends ResourceEndpoint
     public function import(array $body): void
     {
         $this->postRequest('/import', ['json' => $body]);
+    }
+
+    /**
+     * @param mixed[] $body
+     */
+    public function create(array $body): BatchSendingItem
+    {
+        return $this->makeResource($this->putRequest('', ['json' => $body]));
     }
 }
