9507fae8cf05f82efb3c86c1b2c1e362395141c7
IamDejv <kaliankodavid@gmail.com>
2025-11-03 17:08:34 +0100

    Specify `BatchSending.importFields` property type

--- src/Resource/BatchSending.php
+++ src/Resource/BatchSending.php
@@ -32,7 +32,7 @@ class BatchSending extends BaseResource
     public string $status;
 
     /**
-     * @var array<string, string>
+     * @var array<string, array<string>>
      */
     public array $importFields;
 
