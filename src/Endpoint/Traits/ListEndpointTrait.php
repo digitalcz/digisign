c2a78af6386e8fa49d23ce908a109b6b2ede0569
pavelvais <pavel.vais@digital.cz>
2026-09-21 21:40:27 +0200

    Remove phpdoc

--- src/Endpoint/Traits/ListEndpointTrait.php
+++ src/Endpoint/Traits/ListEndpointTrait.php
@@ -12,8 +12,6 @@ use DigitalCz\DigiSign\Resource\ListResource;
 trait ListEndpointTrait
 {
     /**
-     * `_actions` / `_links` already present in $query take precedence over the arguments and are sent once.
-     *
      * @param mixed[] $query
      * @param bool $actions Include `_actions` on each item (set false to speed up large lists)
      * @param bool $links Include `_links` on each item
